c6119b1f2c83065c84dfa0de110b9036cbd0f3d0
Roger Garcia <96830104+rogergarciapages@users.noreply.github.com>
2024-11-25 09:23:35 +0000

    navbar fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,3 @@
-// components/navbar.tsx
 'use client'
 
 import {
@@ -23,22 +22,33 @@ import { categories } from '@/app/config/categories'
 import { Search } from 'lucide-react'
 import { UserNav } from './user-nav'
 import type { Route } from 'next'
+import { useState } from 'react'
 
 export function Navbar() {
   const { data: session } = useSession()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+    <NextUINavbar 
+      maxWidth="xl" 
+      position="sticky"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+
+      <NavbarContent className="sm:flex basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <Link href={{ pathname: '/' }} className="flex justify-start items-center gap-1">
             <Image
-              src="/images/logo.svg"
+              src="/images/logo-p.svg"
               alt="Modern Magazine Logo"
               width={52}
               height={52}
+              className="block" // Ensure logo is always visible
             />
-            
           </Link>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -76,12 +86,10 @@ export function Navbar() {
               </DropdownMenu>
             </Dropdown>
           ))}
-
         </ul>
       </NavbarContent>
 
       <NavbarContent justify="end">
-
         <NavbarItem className="hidden sm:flex">
           <ThemeToggle />
         </NavbarItem>
